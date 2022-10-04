1ff48b04f84ab9efc1bf9c154d8494fceb7f5460
zx <15127662083@163.com>
2022-10-04 10:19:04 +0800

    增加部分功能

--- src/pages/BookInfo.vue
+++ src/pages/BookInfo.vue
@@ -1,117 +1,282 @@
 <template>
-  <div class="container list">
-    <div class="container-info-panel rowup">
-      <el-row :gutter="20" class="panel-row">
-        <el-col :span="4" class="border">测试1</el-col>
-        <el-col :span="4" class="border">测试2</el-col>
-        <el-col :span="4" class="border">测试3</el-col>
-        <el-col :span="4" class="border">测试4</el-col>
-        <el-col :span="4" class="border">测试5</el-col>
-        <el-col :span="4" class="border">测试6</el-col>
-      </el-row>
-      <el-row :gutter="20" class="panel-row">
-        <el-col :span="4" class="border">1</el-col>
-        <el-col :span="6" class="border">2</el-col>
-        <el-col :span="4" class="border">4</el-col>
-        <el-col :span="6" class="border">5</el-col>
-        <el-col :span="4" class="border">6</el-col>
-      </el-row>
-      <el-row :gutter="20" class="panel-row">
-        <el-col :span="4" class="border">1</el-col>
-        <el-col :span="4" class="border">2</el-col>
-        <el-col :span="4" class="border">3</el-col>
-        <el-col :span="4" class="border">4</el-col>
-        <el-col :span="4" class="border">5</el-col>
-        <el-col :span="4" class="border">6</el-col>
-      </el-row>
-      <el-row :gutter="20" class="panel-row">
-        <el-col :span="4" class="border">1</el-col>
-        <el-col :span="6" class="border">2</el-col>
-        <el-col :span="4" class="border">4</el-col>
-        <el-col :span="6" class="border">5</el-col>
-        <el-col :span="4" class="border">6</el-col>
-      </el-row>
-      <el-row :gutter="20" class="panel-row">
-        <el-col :span="4" class="border">1</el-col>
-        <el-col :span="4" class="border">2</el-col>
-        <el-col :span="4" class="border">3</el-col>
-        <el-col :span="4" class="border">4</el-col>
-        <el-col :span="4" class="border">5</el-col>
-        <el-col :span="4" class="border">6</el-col>
-      </el-row>
-      <el-row :gutter="20" class="panel-row">
-        <el-col :span="4" class="border">1</el-col>
-        <el-col :span="6" class="border">2</el-col>
-        <el-col :span="4" class="border">4</el-col>
-        <el-col :span="6" class="border">5</el-col>
-        <el-col :span="4" class="border">6</el-col>
-      </el-row>
-      <el-row :gutter="20" class="panel-row">
-        <el-col :span="4" class="border">1</el-col>
-        <el-col :span="4" class="border">2</el-col>
-        <el-col :span="4" class="border">3</el-col>
-        <el-col :span="4" class="border">4</el-col>
-        <el-col :span="4" class="border">5</el-col>
-        <el-col :span="4" class="border">6</el-col>
-      </el-row>
-      <el-row :gutter="20" class="panel-row">
-        <el-col :span="4" class="border">1</el-col>
-        <el-col :span="6" class="border">2</el-col>
-        <el-col :span="4" class="border">4</el-col>
-        <el-col :span="6" class="border">5</el-col>
-        <el-col :span="4" class="border">6</el-col>
-      </el-row>
-      <el-row :gutter="20" class="panel-row">
-        <el-col :span="4" class="border">1</el-col>
-        <el-col :span="4" class="border">2</el-col>
-        <el-col :span="4" class="border">3</el-col>
-        <el-col :span="4" class="border">4</el-col>
-        <el-col :span="4" class="border">5</el-col>
-        <el-col :span="4" class="border">6</el-col>
-      </el-row>
-      <el-row :gutter="20" class="panel-row">
-        <el-col :span="4" class="border">1</el-col>
-        <el-col :span="6" class="border">2</el-col>
-        <el-col :span="4" class="border">4</el-col>
-        <el-col :span="6" class="border">5</el-col>
-        <el-col :span="4" class="border">6</el-col>
-      </el-row>
-      <el-row :gutter="20" class="panel-row">
-        <el-col :span="4" class="border">1</el-col>
-        <el-col :span="4" class="border">2</el-col>
-        <el-col :span="4" class="border">3</el-col>
-        <el-col :span="4" class="border">4</el-col>
-        <el-col :span="4" class="border">5</el-col>
-        <el-col :span="4" class="border">6</el-col>
-      </el-row>
-      <el-row :gutter="20" class="panel-row">
-        <el-col :span="4" class="border">1</el-col>
-        <el-col :span="6" class="border">2</el-col>
-        <el-col :span="4" class="border">4</el-col>
-        <el-col :span="6" class="border">5</el-col>
-        <el-col :span="4" class="border">6</el-col>
-      </el-row>
-      <el-row :gutter="20" class="panel-row">
-        <el-col :span="4" class="border">1</el-col>
-        <el-col :span="4" class="border">2</el-col>
-        <el-col :span="4" class="border">3</el-col>
-        <el-col :span="4" class="border">4</el-col>
-        <el-col :span="4" class="border">5</el-col>
-        <el-col :span="4" class="border">6</el-col>
-      </el-row>
-      <el-row :gutter="20" class="panel-row">
-        <el-col :span="4" class="border">最后1</el-col>
-        <el-col :span="6" class="border">最后2</el-col>
-        <el-col :span="4" class="border">最后4</el-col>
-        <el-col :span="6" class="border">最后5</el-col>
-        <el-col :span="4" class="border">最后6</el-col>
-      </el-row>
+  <div class="container">
+    <div class="container-panel" v-for=" (item,index) in bookList" :key="index">
+      <div class="container-info-panel" :style="{'marginTop':marginTop+'px'}">
+        <div class="book-img">
+          <img :src="item.bookImg">
+        </div>
+        <div class="book-info">
+          {{item.bookInfo}}
+        </div>
+      </div>
     </div>
-
   </div>
 </template>
 
 <script>
+export default {
+  data() {
+    return{
+      bookList:[{
+        bookImg:'https://images-1309592807.cos.ap-beijing.myqcloud.com/images/截屏2022-08-21 16.49.26.png',
+        bookTitle:'测试1',
+        bookInfo:'这里是测试内容，读者拥有一颗美丽的心灵爸爸啊巴巴爸爸巴巴爸爸巴巴爸爸1'
+      },{
+        bookImg:'https://images-1309592807.cos.ap-beijing.myqcloud.com/images/截屏2022-09-05 09.47.38.png',
+        bookTitle:'测试2',
+        bookInfo:'这里是测试内容，读者拥有一颗美丽的心灵爸爸啊巴巴爸爸巴巴爸爸巴巴爸爸2'
+      },{
+        bookImg:'https://images-1309592807.cos.ap-beijing.myqcloud.com/images/截屏2022-08-21 16.49.26.png',
+        bookTitle:'测试4',
+        bookInfo:'这里是测试内容，读者拥有一颗美丽的心灵爸爸啊巴巴爸爸巴巴爸爸巴巴爸爸3'
+      },{
+        bookImg:'https://images-1309592807.cos.ap-beijing.myqcloud.com/images/截屏2022-08-21 16.49.26.png',
+        bookTitle:'测试5',
+        bookInfo:'这里是测试内容，读者拥有一颗美丽的心灵爸爸啊巴巴爸爸巴巴爸爸巴巴爸爸4'
+      },{
+        bookImg:'https://images-1309592807.cos.ap-beijing.myqcloud.com/images/截屏2022-08-21 16.49.26.png',
+        bookTitle:'测试6',
+        bookInfo:'这里是测试内容，读者拥有一颗美丽的心灵爸爸啊巴巴爸爸巴巴爸爸巴巴爸爸5'
+      },{
+        bookImg:'https://images-1309592807.cos.ap-beijing.myqcloud.com/images/截屏2022-08-21 16.49.26.png',
+        bookTitle:'测试7',
+        bookInfo:'这里是测试内容，读者拥有一颗美丽的心灵爸爸啊巴巴爸爸巴巴爸爸巴巴爸爸6'
+      },{
+        bookImg:'https://images-1309592807.cos.ap-beijing.myqcloud.com/images/截屏2022-08-21 16.49.26.png',
+        bookTitle:'测试8',
+        bookInfo:'这里是测试内容，读者拥有一颗美丽的心灵爸爸啊巴巴爸爸巴巴爸爸巴巴爸爸7'
+      },{
+        bookImg:'https://images-1309592807.cos.ap-beijing.myqcloud.com/images/截屏2022-08-21 16.49.26.png',
+        bookTitle:'测试9',
+        bookInfo:'这里是测试内容，读者拥有一颗美丽的心灵爸爸啊巴巴爸爸巴巴爸爸巴巴爸爸8'
+      },{
+        bookImg:'https://images-1309592807.cos.ap-beijing.myqcloud.com/images/截屏2022-08-21 16.49.26.png',
+        bookTitle:'测试10',
+        bookInfo:'这里是测试内容，读者拥有一颗美丽的心灵爸爸啊巴巴爸爸巴巴爸爸巴巴爸爸9'
+      },{
+        bookImg:'https://images-1309592807.cos.ap-beijing.myqcloud.com/images/截屏2022-08-21 16.49.26.png',
+        bookTitle:'测试11',
+        bookInfo:'这里是测试内容，读者拥有一颗美丽的心灵爸爸啊巴巴爸爸巴巴爸爸巴巴爸爸10'
+      },{
+        bookImg:'https://images-1309592807.cos.ap-beijing.myqcloud.com/images/截屏2022-08-21 16.49.26.png',
+        bookTitle:'测试11',
+        bookInfo:'这里是测试内容，读者拥有一颗美丽的心灵爸爸啊巴巴爸爸巴巴爸爸巴巴爸爸10'
+      },{
+        bookImg:'https://images-1309592807.cos.ap-beijing.myqcloud.com/images/截屏2022-08-21 16.49.26.png',
+        bookTitle:'测试11',
+        bookInfo:'这里是测试内容，读者拥有一颗美丽的心灵爸爸啊巴巴爸爸巴巴爸爸巴巴爸爸10'
+      },{
+        bookImg:'https://images-1309592807.cos.ap-beijing.myqcloud.com/images/截屏2022-08-21 16.49.26.png',
+        bookTitle:'测试11',
+        bookInfo:'这里是测试内容，读者拥有一颗美丽的心灵爸爸啊巴巴爸爸巴巴爸爸巴巴爸爸10'
+      },{
+        bookImg:'https://images-1309592807.cos.ap-beijing.myqcloud.com/images/截屏2022-08-21 16.49.26.png',
+        bookTitle:'测试11',
+        bookInfo:'这里是测试内容，读者拥有一颗美丽的心灵爸爸啊巴巴爸爸巴巴爸爸巴巴爸爸10'
+      },{
+        bookImg:'https://images-1309592807.cos.ap-beijing.myqcloud.com/images/截屏2022-08-21 16.49.26.png',
+        bookTitle:'测试11',
+        bookInfo:'这里是测试内容，读者拥有一颗美丽的心灵爸爸啊巴巴爸爸巴巴爸爸巴巴爸爸10'
+      },{
+        bookImg:'https://images-1309592807.cos.ap-beijing.myqcloud.com/images/截屏2022-08-21 16.49.26.png',
+        bookTitle:'测试11',
+        bookInfo:'这里是测试内容，读者拥有一颗美丽的心灵爸爸啊巴巴爸爸巴巴爸爸巴巴爸爸10'
+      },{
+        bookImg:'https://images-1309592807.cos.ap-beijing.myqcloud.com/images/截屏2022-08-21 16.49.26.png',
+        bookTitle:'测试11',
+        bookInfo:'这里是测试内容，读者拥有一颗美丽的心灵爸爸啊巴巴爸爸巴巴爸爸巴巴爸爸10'
+      },{
+        bookImg:'https://images-1309592807.cos.ap-beijing.myqcloud.com/images/截屏2022-08-21 16.49.26.png',
+        bookTitle:'测试11',
+        bookInfo:'这里是测试内容，读者拥有一颗美丽的心灵爸爸啊巴巴爸爸巴巴爸爸巴巴爸爸10'
+      },{
+        bookImg:'https://images-1309592807.cos.ap-beijing.myqcloud.com/images/截屏2022-08-21 16.49.26.png',
+        bookTitle:'测试11',
+        bookInfo:'这里是测试内容，读者拥有一颗美丽的心灵爸爸啊巴巴爸爸巴巴爸爸巴巴爸爸10'
+      },{
+        bookImg:'https://images-1309592807.cos.ap-beijing.myqcloud.com/images/截屏2022-08-21 16.49.26.png',
+        bookTitle:'测试11',
+        bookInfo:'这里是测试内容，读者拥有一颗美丽的心灵爸爸啊巴巴爸爸巴巴爸爸巴巴爸爸10'
+      },{
+        bookImg:'https://images-1309592807.cos.ap-beijing.myqcloud.com/images/截屏2022-08-21 16.49.26.png',
+        bookTitle:'测试11',
+        bookInfo:'这里是测试内容，读者拥有一颗美丽的心灵爸爸啊巴巴爸爸巴巴爸爸巴巴爸爸10'
+      },{
+        bookImg:'https://images-1309592807.cos.ap-beijing.myqcloud.com/images/截屏2022-08-21 16.49.26.png',
+        bookTitle:'测试11',
+        bookInfo:'这里是测试内容，读者拥有一颗美丽的心灵爸爸啊巴巴爸爸巴巴爸爸巴巴爸爸10'
+      },{
+        bookImg:'https://images-1309592807.cos.ap-beijing.myqcloud.com/images/截屏2022-08-21 16.49.26.png',
+        bookTitle:'测试11',
+        bookInfo:'这里是测试内容，读者拥有一颗美丽的心灵爸爸啊巴巴爸爸巴巴爸爸巴巴爸爸10'
+      },{
+        bookImg:'https://images-1309592807.cos.ap-beijing.myqcloud.com/images/截屏2022-08-21 16.49.26.png',
+        bookTitle:'测试11',
+        bookInfo:'这里是测试内容，读者拥有一颗美丽的心灵爸爸啊巴巴爸爸巴巴爸爸巴巴爸爸10'
+      },{
+        bookImg:'https://images-1309592807.cos.ap-beijing.myqcloud.com/images/截屏2022-08-21 16.49.26.png',
+        bookTitle:'测试11',
+        bookInfo:'这里是测试内容，读者拥有一颗美丽的心灵爸爸啊巴巴爸爸巴巴爸爸巴巴爸爸10'
+      },{
+        bookImg:'https://images-1309592807.cos.ap-beijing.myqcloud.com/images/截屏2022-08-21 16.49.26.png',
+        bookTitle:'测试11',
+        bookInfo:'这里是测试内容，读者拥有一颗美丽的心灵爸爸啊巴巴爸爸巴巴爸爸巴巴爸爸10'
+      },{
+        bookImg:'https://images-1309592807.cos.ap-beijing.myqcloud.com/images/截屏2022-08-21 16.49.26.png',
+        bookTitle:'测试11',
+        bookInfo:'这里是测试内容，读者拥有一颗美丽的心灵爸爸啊巴巴爸爸巴巴爸爸巴巴爸爸10'
+      },{
+        bookImg:'https://images-1309592807.cos.ap-beijing.myqcloud.com/images/截屏2022-08-21 16.49.26.png',
+        bookTitle:'测试11',
+        bookInfo:'这里是测试内容，读者拥有一颗美丽的心灵爸爸啊巴巴爸爸巴巴爸爸巴巴爸爸10'
+      },{
+        bookImg:'https://images-1309592807.cos.ap-beijing.myqcloud.com/images/截屏2022-08-21 16.49.26.png',
+        bookTitle:'测试11',
+        bookInfo:'这里是测试内容，读者拥有一颗美丽的心灵爸爸啊巴巴爸爸巴巴爸爸巴巴爸爸10'
+      },{
+        bookImg:'https://images-1309592807.cos.ap-beijing.myqcloud.com/images/截屏2022-08-21 16.49.26.png',
+        bookTitle:'测试11',
+        bookInfo:'这里是测试内容，读者拥有一颗美丽的心灵爸爸啊巴巴爸爸巴巴爸爸巴巴爸爸10'
+      },{
+        bookImg:'https://images-1309592807.cos.ap-beijing.myqcloud.com/images/截屏2022-08-21 16.49.26.png',
+        bookTitle:'测试11',
+        bookInfo:'这里是测试内容，读者拥有一颗美丽的心灵爸爸啊巴巴爸爸巴巴爸爸巴巴爸爸10'
+      },{
+        bookImg:'https://images-1309592807.cos.ap-beijing.myqcloud.com/images/截屏2022-08-21 16.49.26.png',
+        bookTitle:'测试11',
+        bookInfo:'这里是测试内容，读者拥有一颗美丽的心灵爸爸啊巴巴爸爸巴巴爸爸巴巴爸爸10'
+      },{
+        bookImg:'https://images-1309592807.cos.ap-beijing.myqcloud.com/images/截屏2022-08-21 16.49.26.png',
+        bookTitle:'测试11',
+        bookInfo:'这里是测试内容，读者拥有一颗美丽的心灵爸爸啊巴巴爸爸巴巴爸爸巴巴爸爸10'
+      },{
+        bookImg:'https://images-1309592807.cos.ap-beijing.myqcloud.com/images/截屏2022-08-21 16.49.26.png',
+        bookTitle:'测试11',
+        bookInfo:'这里是测试内容，读者拥有一颗美丽的心灵爸爸啊巴巴爸爸巴巴爸爸巴巴爸爸10'
+      },{
+        bookImg:'https://images-1309592807.cos.ap-beijing.myqcloud.com/images/截屏2022-08-21 16.49.26.png',
+        bookTitle:'测试11',
+        bookInfo:'这里是测试内容，读者拥有一颗美丽的心灵爸爸啊巴巴爸爸巴巴爸爸巴巴爸爸10'
+      },{
+        bookImg:'https://images-1309592807.cos.ap-beijing.myqcloud.com/images/截屏2022-08-21 16.49.26.png',
+        bookTitle:'测试11',
+        bookInfo:'这里是测试内容，读者拥有一颗美丽的心灵爸爸啊巴巴爸爸巴巴爸爸巴巴爸爸10'
+      },{
+        bookImg:'https://images-1309592807.cos.ap-beijing.myqcloud.com/images/截屏2022-08-21 16.49.26.png',
+        bookTitle:'测试11',
+        bookInfo:'这里是测试内容，读者拥有一颗美丽的心灵爸爸啊巴巴爸爸巴巴爸爸巴巴爸爸10'
+      },{
+        bookImg:'https://images-1309592807.cos.ap-beijing.myqcloud.com/images/截屏2022-08-21 16.49.26.png',
+        bookTitle:'测试11',
+        bookInfo:'这里是测试内容，读者拥有一颗美丽的心灵爸爸啊巴巴爸爸巴巴爸爸巴巴爸爸10'
+      },{
+        bookImg:'https://images-1309592807.cos.ap-beijing.myqcloud.com/images/截屏2022-08-21 16.49.26.png',
+        bookTitle:'测试11',
+        bookInfo:'这里是测试内容，读者拥有一颗美丽的心灵爸爸啊巴巴爸爸巴巴爸爸巴巴爸爸10'
+      },{
+        bookImg:'https://images-1309592807.cos.ap-beijing.myqcloud.com/images/截屏2022-08-21 16.49.26.png',
+        bookTitle:'测试11',
+        bookInfo:'这里是测试内容，读者拥有一颗美丽的心灵爸爸啊巴巴爸爸巴巴爸爸巴巴爸爸10'
+      },{
+        bookImg:'https://images-1309592807.cos.ap-beijing.myqcloud.com/images/截屏2022-08-21 16.49.26.png',
+        bookTitle:'测试11',
+        bookInfo:'这里是测试内容，读者拥有一颗美丽的心灵爸爸啊巴巴爸爸巴巴爸爸巴巴爸爸10'
+      },{
+        bookImg:'https://images-1309592807.cos.ap-beijing.myqcloud.com/images/截屏2022-08-21 16.49.26.png',
+        bookTitle:'测试11',
+        bookInfo:'这里是测试内容，读者拥有一颗美丽的心灵爸爸啊巴巴爸爸巴巴爸爸巴巴爸爸10'
+      },{
+        bookImg:'https://images-1309592807.cos.ap-beijing.myqcloud.com/images/截屏2022-08-21 16.49.26.png',
+        bookTitle:'测试11',
+        bookInfo:'这里是测试内容，读者拥有一颗美丽的心灵爸爸啊巴巴爸爸巴巴爸爸巴巴爸爸10'
+      },{
+        bookImg:'https://images-1309592807.cos.ap-beijing.myqcloud.com/images/截屏2022-08-21 16.49.26.png',
+        bookTitle:'测试11',
+        bookInfo:'这里是测试内容，读者拥有一颗美丽的心灵爸爸啊巴巴爸爸巴巴爸爸巴巴爸爸10'
+      },{
+        bookImg:'https://images-1309592807.cos.ap-beijing.myqcloud.com/images/截屏2022-08-21 16.49.26.png',
+        bookTitle:'测试11',
+        bookInfo:'这里是测试内容，读者拥有一颗美丽的心灵爸爸啊巴巴爸爸巴巴爸爸巴巴爸爸10'
+      },{
+        bookImg:'https://images-1309592807.cos.ap-beijing.myqcloud.com/images/截屏2022-08-21 16.49.26.png',
+        bookTitle:'测试11',
+        bookInfo:'这里是测试内容，读者拥有一颗美丽的心灵爸爸啊巴巴爸爸巴巴爸爸巴巴爸爸10'
+      },{
+        bookImg:'https://images-1309592807.cos.ap-beijing.myqcloud.com/images/截屏2022-08-21 16.49.26.png',
+        bookTitle:'测试11',
+        bookInfo:'这里是测试内容，读者拥有一颗美丽的心灵爸爸啊巴巴爸爸巴巴爸爸巴巴爸爸10'
+      },{
+        bookImg:'https://images-1309592807.cos.ap-beijing.myqcloud.com/images/截屏2022-08-21 16.49.26.png',
+        bookTitle:'测试11',
+        bookInfo:'这里是测试内容，读者拥有一颗美丽的心灵爸爸啊巴巴爸爸巴巴爸爸巴巴爸爸10'
+      },{
+        bookImg:'https://images-1309592807.cos.ap-beijing.myqcloud.com/images/截屏2022-08-21 16.49.26.png',
+        bookTitle:'测试11',
+        bookInfo:'这里是测试内容，读者拥有一颗美丽的心灵爸爸啊巴巴爸爸巴巴爸爸巴巴爸爸10'
+      },{
+        bookImg:'https://images-1309592807.cos.ap-beijing.myqcloud.com/images/截屏2022-08-21 16.49.26.png',
+        bookTitle:'测试11',
+        bookInfo:'这里是测试内容，读者拥有一颗美丽的心灵爸爸啊巴巴爸爸巴巴爸爸巴巴爸爸10'
+      },{
+        bookImg:'https://images-1309592807.cos.ap-beijing.myqcloud.com/images/截屏2022-08-21 16.49.26.png',
+        bookTitle:'测试11',
+        bookInfo:'这里是测试内容，读者拥有一颗美丽的心灵爸爸啊巴巴爸爸巴巴爸爸巴巴爸爸10'
+      },{
+        bookImg:'https://images-1309592807.cos.ap-beijing.myqcloud.com/images/截屏2022-08-21 16.49.26.png',
+        bookTitle:'测试11',
+        bookInfo:'这里是测试内容，读者拥有一颗美丽的心灵爸爸啊巴巴爸爸巴巴爸爸巴巴爸爸10'
+      },{
+        bookImg:'https://images-1309592807.cos.ap-beijing.myqcloud.com/images/截屏2022-08-21 16.49.26.png',
+        bookTitle:'测试11',
+        bookInfo:'这里是测试内容，读者拥有一颗美丽的心灵爸爸啊巴巴爸爸巴巴爸爸巴巴爸爸10'
+      },{
+        bookImg:'https://images-1309592807.cos.ap-beijing.myqcloud.com/images/截屏2022-08-21 16.49.26.png',
+        bookTitle:'测试11',
+        bookInfo:'这里是测试内容，读者拥有一颗美丽的心灵爸爸啊巴巴爸爸巴巴爸爸巴巴爸爸10'
+      },{
+        bookImg:'https://images-1309592807.cos.ap-beijing.myqcloud.com/images/截屏2022-08-21 16.49.26.png',
+        bookTitle:'测试11',
+        bookInfo:'这里是测试内容，读者拥有一颗美丽的心灵爸爸啊巴巴爸爸巴巴爸爸巴巴爸爸10'
+      },{
+        bookImg:'https://images-1309592807.cos.ap-beijing.myqcloud.com/images/截屏2022-08-21 16.49.26.png',
+        bookTitle:'测试11',
+        bookInfo:'这里是测试内容，读者拥有一颗美丽的心灵爸爸啊巴巴爸爸巴巴爸爸巴巴爸爸10'
+      },{
+        bookImg:'https://images-1309592807.cos.ap-beijing.myqcloud.com/images/截屏2022-08-21 16.49.26.png',
+        bookTitle:'测试11',
+        bookInfo:'这里是测试内容，读者拥有一颗美丽的心灵爸爸啊巴巴爸爸巴巴爸爸巴巴爸爸10'
+      },{
+        bookImg:'https://images-1309592807.cos.ap-beijing.myqcloud.com/images/截屏2022-08-21 16.49.26.png',
+        bookTitle:'测试11',
+        bookInfo:'这里是测试内容，读者拥有一颗美丽的心灵爸爸啊巴巴爸爸巴巴爸爸巴巴爸爸10'
+      },{
+        bookImg:'https://images-1309592807.cos.ap-beijing.myqcloud.com/images/截屏2022-08-21 16.49.26.png',
+        bookTitle:'测试11',
+        bookInfo:'这里是测试内容，读者拥有一颗美丽的心灵爸爸啊巴巴爸爸巴巴爸爸巴巴爸爸10'
+      },{
+        bookImg:'https://images-1309592807.cos.ap-beijing.myqcloud.com/images/截屏2022-08-21 16.49.26.png',
+        bookTitle:'测试11',
+        bookInfo:'这里是测试内容，读者拥有一颗美丽的心灵爸爸啊巴巴爸爸巴巴爸爸巴巴爸爸10'
+      }],
+      marginTop:0,
+      timer:'' // 定时器时间
+    }
+  },
+  created() {
+    this.timer = setInterval(this.showNotice, 1000);
+  },
+  methods:{
+    // 数据滚动
+    showNotice(){
+      this.marginTop -= 1;
+      if (this.marginTop < -1){ // 每个盒子的高度
+        this.bookList.push(this.bookList[0]);
+        this.bookList.shift();
+        this.marginTop = 0;
+      }
+    }
+  }
+}
 
 </script>
 
